10331f79eff262ff7e8612d7a5a8060b49ac234e
sanzang-tang <sanzang-tang@outlook.com>
2024-07-21 19:15:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,11 +7,11 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '18ad4c78-6c4c-42f7-8198-60866f062dbc';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+const proxyIPs= ['proxyip.sg.fxxk.dedyn.io', 'sg.ipdb.rr.nu', 'sg.cf.zhetengsha.eu.org'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
@@ -199,8 +199,8 @@ export default {
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.speedtest.net';
+                        return new Response('Not found', { status: 404 });
+                        url.hostname = 'www.bing.com';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
